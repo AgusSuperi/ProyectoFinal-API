027d4a14c5f1535f3face6137badc676507c0c9a
Matias Gonzalez <magonzalez@hexacta.com>
2020-10-22 19:43:15 -0300

    Se agregaron imagenes de algunos caps y se creo el controlador de imagenes

---
+++ ProyectoFinal.API/Controllers/ImagenesController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoFinal.API.Controllers
+{
+    [Route("api/imagenes")]
+    [ApiController]
+    public class ImagenesController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public ImagenesController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        [HttpGet("{nombre}")]
+        public IActionResult GetImagenCaps(string nombre)
+        {
+            return AbrirImagenODefault(nombre);
+        }
+
+        private IActionResult AbrirImagenODefault(string nombre)
+        {
+            var rutaImagen = Path.Combine(_webHostEnvironment.ContentRootPath, "Imagenes/CAPS/" + nombre);
+
+            if (System.IO.File.Exists(rutaImagen))
+            {
+                Byte[] b = System.IO.File.ReadAllBytes(rutaImagen);
+                return File(b, "image/png");
+            }
+            return AbrirImagenPorDefault("defaultCaps.png");
+        }
+
+        private IActionResult AbrirImagenPorDefault(string nombreImagenDefault)
+        {
+            var rutaImagen = Path.Combine(_webHostEnvironment.ContentRootPath, "Imagenes/Default/" + nombreImagenDefault);
+
+            if (System.IO.File.Exists(rutaImagen))
+            {
+                Byte[] b = System.IO.File.ReadAllBytes(rutaImagen);
+                return File(b, "image/png");
+            }
+            return BadRequest();
+        }
+    }
+}
